db9013d535af82e2eaa0cf87d8d9d8b76ddae81f
dorkele <dora_tominic@hotmail.com>
2020-06-11 16:37:24 +0200

    added icons, links and favicon

--- src/Info.js
+++ src/Info.js
@@ -7,22 +7,65 @@ export default function Info({ setInfo, info }) {
     };
     return (
         <div className={styles.container}>
-            <div onClick={closeInfo} className={styles.iks}>
-                X
-            </div>
+            <img
+                src="/images/NB_icons-01.png"
+                alt="x"
+                onClick={closeInfo}
+                className={styles.iks}
+            />
             {info === "about" && (
                 <div className={styles.info}>
                     Nina Bencic /ben-cheech/ freelance Art Director & Designer
                     based in Berlin. With a focus on branding and newly found
-                    interest in typography. Working as a Visual designer at
-                    Goldener Westen↗.
+                    interest in typography. Working as a Visual designer at{" "}
+                    <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href="https://www.goldenerwesten.net"
+                        className={styles.links}
+                    >
+                        Goldener Westen
+                        <img
+                            src="/images/NB_icons-02.png"
+                            alt="arrow icon"
+                            className={styles.icon}
+                        />
+                    </a>
                 </div>
             )}
             {info === "contact" && (
                 <div className={styles.info}>
-                    <p>info[at]ninabencic.com↗</p>
-                    <p>Linkedin↗</p>
-                    <p>Instagram↗</p>
+                    <p>info[at]ninabencic.com</p>
+                    <p>
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://www.linkedin.com/in/nina-bencic-88645763/"
+                            className={styles.links}
+                        >
+                            Linkedin
+                            <img
+                                src="/images/NB_icons-02.png"
+                                alt="arrow icon"
+                                className={styles.icon}
+                            />
+                        </a>
+                    </p>
+                    <p>
+                        <a
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            href="https://www.instagram.com/ninaninabencic/"
+                            className={styles.links}
+                        >
+                            Instagram
+                            <img
+                                src="/images/NB_icons-02.png"
+                                alt="arrow icon"
+                                className={styles.icon}
+                            />
+                        </a>
+                    </p>
                 </div>
             )}
         </div>
